97df6d67dad75e07b1ecdb8dd321f1262f3b2df7
jjwyse <josh@cloud-elements.com>
2014-05-03 15:43:32 -0600

    Working on the projects page

--- routes/projects.js
+++ routes/projects.js
@@ -1,6 +1,25 @@
-/*
- * GET all projects
- */
-exports.list = function (req, res) {
-   res.send('{hello:world}')
+var https = require('https');
+
+exports.list = function(req, res) {
+   res.send('todo');
+}
+
+exports.apiList = function (req, res) {
+
+   var options = {
+      host: 'api.github.com',
+      path: '/users/jjwyse/repos',
+      headers: {'User-Agent:': 'Personal Website'}
+   };
+
+   https.get(options, function(res) {
+      console.log("status code:" + res.statusCode);
+      console.log("headers:" + res.headers);
+
+      res.on('data', function(d){
+         process.stdout.write(d);
+      });
+   }).on('error', function(e){
+      console.error(e);
+   });
 };
