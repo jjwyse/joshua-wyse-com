7cd233eaee4058d62bdd261d1a888fd6e6c5081a
jjwyse <josh@cloud-elements.com>
2014-05-07 21:12:59 -0600

    Working on making ajax calls for each tab

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
-config.github = 'd89f578de76330b2b372a26211f55277cadb810e';
 config.mongodb = 'localhost:27017/trivia-scoreboard';
+config.github = 'd89f578de76330b2b372a26211f55277cadb810e';
 config.strava = '51a543276876e867b683ac0bd420624787170d59';
 
 module.exports = config;
--- public/js/joshua-wyse-com.js
+++ public/js/joshua-wyse-com.js
@@ -1,1 +1,19 @@
-todo
+$(document).ready(function(){
+
+   // load all projects
+   $('a#projects').click(function(){
+      console.log('Loading projects...');
+      $.ajax({
+         type:'GET',
+         url:'/api/projects',
+         success:function(json){
+            console.log("Retrieved projects:");
+            alert(json);
+            $.each(json, function(key, project){
+               console.log("Project name: " + project.name);
+            });
+            console.log("Finished loading projects");
+         }
+      });
+   });
+});
