1b0b369a8f613173fdb2f0efaa1cf4f96a59d510
jjwyse <josh@cloud-elements.com>
2014-05-11 21:44:20 -0600

    Working on some javascript to clean up the front-end and asynchronously load the projects and runs

--- public/js/joshua-wyse-com.js
+++ public/js/joshua-wyse-com.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 
+   $('li').click(function(){
+      $('.content').fadeOut('slow');
+      $('li').removeClass('active');
+      $(this).toggleClass('active');
+   });
+
    // load all projects
    $('a#projects').click(function(){
       console.log('Loading projects...');
@@ -8,12 +14,37 @@ $(document).ready(function(){
          url:'/api/projects',
          success:function(json){
             console.log("Retrieved projects:");
+            $('.content').text("");
             $.each(json, function(key, project){
                console.log("Project name: " + project.name);
                $('.content').append(project.name + '<br/>');
             });
             console.log("Finished loading projects");
+            $('.content').fadeIn('fast');
          }
       });
    });
+
+   // load all runs
+   $('a#runs').click(function(){
+      console.log('Loading runs...');
+      $.ajax({
+         type:'GET',
+         url:'/api/runs',
+         success:function(json){
+            console.log("Retrieved runs:");
+            $('.content').text("");
+            $.each(json, function(key, run){
+               console.log("Run name: " + run.name);
+               $('.content').append('<b>' + run.name + '</b>' + ' - ' + metersToMiles(run.distance) + ' miles <br/>');
+            });
+            console.log("Finished loading runs");
+            $('.content').fadeIn('fast');
+         }
+      });
+   });
+
+   function metersToMiles(meters) {
+      return Math.round((meters / 1609.344) * 100) / 100;
+   }
 });
