6792465bcba368d0b5e3f37a5cc996c01de3e395
jjwyse <josh@cloud-elements.com>
2014-05-12 21:07:27 -0600

    Lots of work to views and playing around with js and jade

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var routes = require('./routes');
 var projects = require('./routes/projects');
 var runs = require('./routes/runs');
+var users = require('./routes/users');
 
 var http = require('http');
 var path = require('path');
@@ -17,7 +18,7 @@ var app = express();
 app.set('port', process.env.PORT || 2997);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon("/images/favicon.ico")); 
+app.use(express.favicon("/images/favicon.ico"));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
@@ -33,6 +34,7 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/api/projects', projects.list(config.github))
 app.get('/api/runs', runs.list(config.strava))
+app.get('/api/users/contact', users.contact(db))
 
 http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
