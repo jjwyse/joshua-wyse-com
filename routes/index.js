580552378f3f3768455149906a780ab49186509e
jjwyse <josh@cloud-elements.com>
2014-10-12 16:41:06 -0600

    almost have mail server running

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,17 @@
+var unirest = require('unirest');
+
 exports.index = function (req, res) {
    res.render('index', { title: 'Joshua Wyse' });
 };
 
-exports.sendMessage = function(req, res) {
-  res.send({"success": true});
-}
+exports.sendMessage = function(token, userSecret) {
+  return function(req, res) {
+    console.log(req.body);
+    unirest.post('https://api.cloud-elements.com/elements/api-v2/hubs/messaging/messages')
+      .headers({ 'Content-Type': 'application/json' }, {'Authorization': 'Element ' + token + ', User ' + userSecret})
+      .end(function (response) {
+        console.log(response.body);
+        res.send(response.body);
+      });
+  };
+};
